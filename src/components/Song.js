8c18fcd5e11ecaf08521d86264fabc0a401847f1
Alyson <alysonleonard19@gmail.com>
2024-02-02 20:07:35 -0800

    Add rating stars / count

--- src/components/Song.js
+++ src/components/Song.js
@@ -1,7 +1,7 @@
 import React, { useRef, useState } from "react";
 import './Song.css'
 
-function Song({ band, type, title, rating, username }) {
+function Song({ band, type, title, rating, numRatings, username }) {
 
     const [scrollTitle, setScrollTitle] = useState(false);
     const titleRef = useRef(null);
@@ -13,6 +13,29 @@ function Song({ band, type, title, rating, username }) {
             setScrollTitle(true);
         }
     };
+
+    const getRatingStars = (rating) => {
+        let stars = [];
+        let starSize = 1.2; //rem
+
+        for (let i=0; i<Math.floor(rating); i++) {
+            stars.push(<div className='star' style={{'--star-size' : `${starSize}rem`}}/>);
+        }
+    
+        if (Math.ceil(rating) - Math.floor(rating) > 0) {
+            stars.push(<div className='star half' style={{'--star-size' : `${starSize}rem`}}/>);
+        }
+    
+        stars.push(<div className="num-ratings">{`${numRatings} ratings`}</div>);
+
+        return stars;
+    };
+
+    let ratingStars = [];
+    if (typeof rating === 'number') {
+        if (rating > 5) { rating = 5; }
+        ratingStars = getRatingStars(rating)
+    }
 
     let scrollStyle = {};
     if (scrollTitle) {
@@ -29,7 +52,7 @@ function Song({ band, type, title, rating, username }) {
                 onMouseEnter={() => checkTitleClip()}
                 onMouseLeave={() => { setScrollTitle(false) }}
             >{title}</div>
-            <div className="">{rating}</div>
+            <div className="rating">{ratingStars}</div>
             <div className="">{`@${username}`}</div>
         </div>
     );
--- src/components/SongList.js
+++ src/components/SongList.js
@@ -8,7 +8,7 @@ function SongList({ header, title }) {
             <div className='list-banner' style={{backgroundImage : header}}>
                 <div className="list-title">{title}</div>
             </div>
-            <Song band='Brand New' type='Tab' title="Okay I Believe You But My Tommy Gun Don't" rating={4} username='alleysun19'/>
+            <Song band='Brand New' type='Tab' title="Okay I Believe You But My Tommy Gun Don't" rating={4.5} numRatings= {1234} username='alleysun19'/>
         </div>
     );
 }
